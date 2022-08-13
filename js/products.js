6e2828fcafd09f1c64524fd934e3d66d84ecb206
alina <alinasurjantrofo@gmail.com>
2022-08-13 17:34:53 -0300

    listado json en productos html y j

--- js/products.js
+++ js/products.js
@@ -1,48 +1,40 @@
-//dirección para obtener el listado en formato json:
-const LIST_URL = "https://japdevdep.github.io/ecommerce-api/category/all.json";
+const catProductAutos101 = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
-
-
-let categoriesArray = [];
-
+let categoriesArray =[];
 
 function showCategoriesList(array){
     let htmlContentToAppend = "";
 
-    for(let i = 0; i < array.length; i++){ 
+    for (let i = 0; i <array.length; i++){
         let category = array[i];
         htmlContentToAppend += `
-        <main class="pb-5 container">
-            <div class="container">
+        <div class="list-group-item list-group-item-action cursor-active">
+            <div class="row">
                 <div class="col-3">
-                    <img src="` + category.image + `" alt="product image" class="img-thumbnail">
-                </div>
-                <div class="col">
-                    <div class="d-flex w-100 justify-content-between">
-                        <div class="mb-1">
-                        <h4>`+ category.name +`</h4> 
-                        <p> `+ category.description +`</p> 
-                        <p> `+ category.cost +`</p>
-                        <p> `+ category.currency +`</p>
-                        </div>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small> 
+                    <img src="` + category.image + `" alt="product image" class="imgcatautos">
                     </div>
+                    <div class ="col>
+                        <div class="d-flex w-100 justify-content-between">
+                            <div class="mb-1">
+                            <h4> `+ category.name +`</h>
+                            <p> `+ ctegory.description +`</p>
+                            </div>
+                            <small class="text-muted">` + category.soldCount + `articulos vendidos </small>
+                        </div>
 
-                </div>
-            </div>
-        </main>
-        `
-        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
+                    </div>
+                </div>    
+            </div>    
+            `
+            document.getElementById("cat-list-container").innerHTML = htmlContentToAppend; 
     }
-}
+    }
 
 
 //-JSONData()  showCategoriesList() pasándole por parámetro categoriesArray.
 
-*/
-
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(LIST_URL).then(function(resultObj){
+    getJSONData(catProductAutos101).then(function(resultObj){
         if (resultObj.status === "ok")
         {
             categoriesArray = resultObj.data;

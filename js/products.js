4b39e4350c405469e29ebe5de6c5a849c5b2c86d
alina <alinasurjantrofo@gmail.com>
2022-08-14 23:04:31 -0300

    products

--- js/products.js
+++ js/products.js
@@ -1,32 +1,32 @@
 
 
-let categoriesArray =[];
+let categoriesListado =[];
 
-function showCategoriesList(array){
-    let htmlContentToAppend = "";
+function showCategoriesList(listado){
+    let lista = "";
 
-    for (let i=0 ; i <array.products.length; i++){
-        let category = array[i];
-        htmlContentToAppend += `
+    for (let i=0 ; i <listado.products.length; i++){
+        let vehiculo = listado[i];
+        lista += `
         <div class="list-group-item list-group-item-action cursor-active">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + category.products.image + `" alt="product image" class="img-thumbnail">
+                    <img src="` + vehiculo.image + `" alt="product image" class="img-thumbnail">
                     </div>
                     <div class ="col">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="mb-1">
-                            <h4> `+ category.products.name +`<br> `+ category.products.currency + category.cost +` </h4>
-                            <p> `+ ctegory.products.description +`</p>
+                            <h4> `+ vehiculo.products.name +`<br> `+ vehiculo.products.currency + vehiculo.cost +` </h4>
+                            <p> `+ vehiculo.products.description +`</p>
                             </div>
-                            <small class="text-muted">` + category.products.soldCount + `articulos vendidos </small>
+                            <small class="text-muted">` + vehiculo.products.soldCount + `articulos vendidos </small>
                         </div>
 
                     </div>
                 </div>    
             </div>    
             `
-            document.getElementById("cat-product-container").innerHTML = htmlContentToAppend; 
+            document.getElementById("cat-product-container").innerHTML = lista; 
     }
     }
 
@@ -35,10 +35,11 @@ function showCategoriesList(array){
 
 document.addEventListener("DOMContentLoaded", ()=> {
     getJSONData(AUTOS_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            categoriesArray = resultObj.data;
-            showCategoriesList(categoriesArray);
+        if (resultObj.status === "ok"){ 
+            categoriesListado = resultObj.data;
+            showCategoriesList(categoriesListado);
         }
-    });
-});
+        else  alertify.alert('Alert Message!');
+         
+        }
+    })

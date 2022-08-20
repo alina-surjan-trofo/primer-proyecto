8a63d69cb42806fa916ae436601fd4087007c7bf
alina <alinasurjantrofo@gmail.com>
2022-08-20 15:52:11 -0300

    products completado

--- js/products.js
+++ js/products.js
@@ -2,24 +2,24 @@
 
 let categoriesListado =[];
 
-function showCategoriesList(listado){
-    let lista = "";
+function showCategoriesList(autos){
+    let listaVehiculos = "";
 
-    for (let i=0 ; i <listado.products.length; i++){
-        let vehiculo = listado[i];
-        lista += `
+    for (auto of autos.products){
+        
+        listaVehiculos += `
         <div class="list-group-item list-group-item-action cursor-active">
             <div class="row">
                 <div class="col-3">
-                    <img src="` + vehiculo.image + `" alt="product image" class="img-thumbnail">
+                    <img src="` + auto.image + `" alt="product image" class="img-thumbnail">
                     </div>
                     <div class ="col">
                         <div class="d-flex w-100 justify-content-between">
                             <div class="mb-1">
-                            <h4> `+ vehiculo.products.name +`<br> `+ vehiculo.products.currency + vehiculo.cost +` </h4>
-                            <p> `+ vehiculo.products.description +`</p>
+                            <h4> `+ auto.name +`<br> `+ auto.currency + auto.cost +` </h4>
+                            <p> `+ auto.description +`</p>
                             </div>
-                            <small class="text-muted">` + vehiculo.products.soldCount + `articulos vendidos </small>
+                            <small class="text-muted">` + auto.soldCount + `articulos vendidos </small>
                         </div>
 
                     </div>
@@ -29,18 +29,18 @@ function showCategoriesList(listado){
             `
             
     }
-    document.getElementById("cat-product-container").innerHTML = lista; 
+    document.getElementById("cat-product-container").innerHTML = listaVehiculos; 
 }
 
 
-//-JSONData()  showCategoriesList() pasándole por parámetro categoriesArray.
+//uso el -JSONData() de Initjs / usola info del productsurl dejo que la lista que da el json sea lista de vehiculos y entonces se muestra la misma
 
 document.addEventListener('DOMContentLoaded',()=>{
-    getJSONData(PRODUCTS_URL).then(function(resultObj){ 
+    getJSONData (PRODUCTS_URL).then(function(resultObj){ 
         if (resultObj.status === "ok"){
 
-            categoriesListado = resultObj.data;
-            showCategoriesList(categoriesListado);
+            listaVehiculos = resultObj.data;
+            showCategoriesList(listaVehiculos);
 
         }
     })
